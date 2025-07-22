155770e2c3db64f6a56709dcf2cde8f4062c6aba
Nurmuhammad14456 <nabdujalilov036@gmail.com>
2025-07-22 10:13:40 +0500

    ozgartirildi

---
+++ javoblar.js
@@ -0,0 +1,152 @@
+//O'zgaruvchi turlari ? 5
+/*
+1. let const ni farqi ? 5
+
+2. var let farqi ? 0
+{
+   var a = 5;
+   let b = 6;
+}
+console.log(a); // 5
+// console.log(b); //ni chaqirib bolmaydi
+....................................................................
+
+3. Data types asosiy nechi xil turga bolinadi ? 4
+2 cilga bolinadi primitiv No-primitiv
+....................................................................
+
+4. Primitiv va No-primitiv (refrence) farqi 3?
+primitiv ga Number String Null undevind begind va symbool boolin  no primitivga Object Array Functioin
+....................................................................
+
+5. Accses before intilization 0 ?
+ozgaruvchini bermasdan qiymatni elon qilishlik xato 
+....................................................................
+
+6. Type conversion  haqida malumot bering 0?
+  biadigan 3 xil usuli bor Number Stringni Numberga otqazadi string Numberni stringa otgazatadi Boolein ha yoki yoq ni beradi 
+....................................................................
+
+7. Type conversion necha xil usuli bor 0?
+5 xil usuli bor men biladigan larim 3 ta Number string boolien
+....................................................................
+
+8. Avtomatik type conversion haqida malumot bering 0?
+JavaScriptda ayrim paytlarda JS avtomatik ravishda qiymatni boshqa turga o‘zgartiradi.
+....................................................................
+
+
+9. Math objecti haqida malumot bering 3?
+  Math objecti asosiy matematik amalar bajarish uchun ishlatiladi 
+....................................................................
+  
+10. Truhy and falsy qiymatlarini sanab bering 0 ?
+falsy qiymatlarga "0, -0 0n ""  Null Undefind Nan"
+truhy qolganlari hammasi falsy qiymatga kiradi 
+....................................................................
+
+11. If else haqida malumo bering 5?
+If else vazifasi togri notugri togri bolsa true oqiydi notugri bosa False ga tushadi
+....................................................................
+
+12. Switch case haqida malumot bering 4?
+SwitCh case Kop amalar bajarish uchun ishlatiladi uning qiymatlari  case ga ifadani  qiymati 1 ga teng bolsa case ga tushadi undan keyin break bor u kesib beradi break ishalatilsa pastidagi kocni oqmaydi
+....................................................................
+
+13. Nechi xil asosiy functionlar bor 0?
+5tami 6tita boor men ishlatiladiganlar 3 xil bor  doclarition, Error fantion exoaression function bor
+....................................................................
+
+
+14. Functionlarni bir biridan farqi 0?
+1 chisi declarition Odiy function  hoisting ga ega — ya’ni chaqirishni yuqorida yozsa ham ishlaydi
+2 function exparition hoisting yo‘q — chaqirishni yuqorida yozsa, xatolik beradi functioni ozgaruvchiga biriktirilgan
+3 arrow function thisni meros olmaydi thisdan tashqarini oladi  kopincha collback lar ishlatiladi ishlatilishi zamonaviy
+....................................................................
+
+15. Functionda return ishlashi 0 ?
+return natijani korsatish uchun ishlatiladi returndan keyin kod ishlamaydi 
+....................................................................
+
+16. IIFE haqida malumot bering 0?
+bu yozilgan dan keyin darhol chaqiradigan function 
+(function () {
+  console.log("Bu IIFE");
+})()
+....................................................................
+
+17. Method nima 0?
+Agar function object ichida bo‘lsa — method bo‘ladi. Agar function function ichida bo‘lsa — nested function, method emas.
+....................................................................
+
+18. Stringni split malumot bering 0?
+split  stringni maydalab beradi 
+....................................................................
+
+19. Loop nima 1?
+loop takrorlash operatori 
+....................................................................
+
+20. Loop turlari 0?
+loopni turlari for for-in for-of while do..while 
+....................................................................
+
+21. do while while loopini farqi 2 ?
+do while kamida bir marta ishlaydi  while ni wart togri bolguncha ishlaydi  foor ozing shart berasnan shuni tekshiradi oxirgacha 
+....................................................................
+
+22. for in for of farqi 0?
+for in faqat object ustida ishlaydi for of array yoki string ustida amal bajaradi 
+....................................................................
+
+23. Assigment operatorlarni sanab bering 4?
+24. == === farqi 5 ?
+== manosi odiy tekshirish === qatiy tekshirish
+
+25. ! operatori haqida  0?
+Bu masalan !true desangiz false chiqaradi !false yozsangiz true chiqaradi bolmasachi degan manoni anglatadi 
+....................................................................
+
+26. Array method concat 5 ?
+concat arrayni bir birini qoshadi 
+....................................................................
+
+27. Array method reverce 5 ?
+arrayni teskari qilib beradi 
+....................................................................
+
+28. Array method splice 0 ?
+splice arrayni maydalab beradi 
+....................................................................
+
+29. Array method foreach 0?
+Arrayni tozalaydi ichiga 3 ta argument qabul qiladi "index value arr"
+....................................................................
+
+30. Array foreach methodni boshqa methodalrdan farqi 0?
+forEach har doim element ustida aylanadi yangi array qaytarmaydi
+....................................................................
+
+31. Arrayni map methodi 0?
+har bir elemetni ozgartiradi yangi array qaytaradi
+....................................................................
+
+32. Arrayni some methodi haqida malumot bering 0?
+Some kamida bitta element shartga mos kelsa true qaytaradi  aks xolda folse
+....................................................................
+
+33. Arrayni every methodi haqida malumot bering  0?
+bunday barcha element wartga mos bolsa true aks xolta false
+....................................................................
+
+34. Arrayni find methodi haqida malumot bering  4?
+Shartga mos keladigani birinchi qidiradi 
+....................................................................
+
+35. Arrayni reduce methodi haqida malumot bering 0 ?
+arrayni ichidagi barcha qiymatni hisoblab beradi 
+
+
+
+
+ */
